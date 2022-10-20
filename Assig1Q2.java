fc3efc2b9c1feb99ac09d944f665b1aa81fa6533
Deep Sengupta <105095730+DeepSengupta-M1@users.noreply.github.com>
2022-10-20 22:23:25 +0530

    Update Assig1Q2.java

--- Assig1Q2.java
+++ Assig1Q2.java
@@ -1,5 +1,5 @@
 public class Assig1Q2 {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         int num1 = 5, num2 = 5;
         System.out.println("Sum of the number is: num1 + num2 = " +(num1+num2));
     }
