a3f185625cacfc408d1783920a90c0d476655baf
Deep Sengupta <105095730+DeepSengupta-M1@users.noreply.github.com>
2022-10-21 19:45:58 +0530

    Add files via upload

---
+++ Assig1Q10.java
@@ -0,0 +1,13 @@
+public class Assig1Q10 {
+    public static void main(String[] args){
+        int num1 = 0, num2 = 1, total = num1+num2;
+        System.out.println("Fibonacci series: " +num1+ "," +num2+ ",");
+        for(int i=0; i<10; i++)
+        {
+           total = num1 + num2;
+           num1 = num2;
+           num2 = total;
+           System.out.println(total + ",");
+        }
+    }
+}
---
+++ Assig1Q9.java
@@ -0,0 +1,19 @@
+class Assig1Q9{  
+    public static void main(String args[]){
+        int i = 10;
+        {
+            int m = 2;
+            System.out.println(i);
+            System.out.println(m);
+        }
+        {
+            int n = 3;
+            System.out.println(i);
+            System.out.println(m);
+            System.out.println(n);
+        }
+    System.out.println(i);
+    System.out.println(m);
+    System.out.println(n);
+    }
+}
