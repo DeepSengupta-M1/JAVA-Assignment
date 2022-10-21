bc49bc56462b3c170aa6315352d0e011ce303633
Deep Sengupta <105095730+DeepSengupta-M1@users.noreply.github.com>
2022-10-21 19:41:30 +0530

    Update Assig1Q4.java

--- Assig1Q4.java
+++ Assig1Q4.java
@@ -1,9 +1,9 @@
 public class Assig1Q4 {
     public static void main(String[] args){
-        int n1=5, n2=15, n3=25;
-        if(n1<n2 && n1<n3)
+        int n1 = 5, n2 = 15, n3 = 25;
+        if (n1<n2 && n1<n3)
         System.out.println("n1 is the smallest number " +n1);
-        else if(n2<n1 && n2<n3)
+        else if (n2<n1 && n2<n3)
         System.out.println("n2 is the smallest number " +n2);
         else 
         System.out.println("n3 is the smallest number " +n3);
