4a5a81da64517b8a38e779ee00bccd137df28a08
Deep Sengupta <105095730+DeepSengupta-M1@users.noreply.github.com>
2022-10-21 19:42:59 +0530

    Update Assig1Q8.java

--- Assig1Q8.java
+++ Assig1Q8.java
@@ -1,6 +1,6 @@
 class Assig1Q8 {
     public static void main(String[] args){
-       double r=75.95;
+       double r = 75.95;
        System.out.println("Price after converting rupees to paise: " +(r*100));
     }
 }
