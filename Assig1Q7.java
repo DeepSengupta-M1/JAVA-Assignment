1dbcf1ffa2e8007bad84f214cad9ccedde982fa3
Deep Sengupta <105095730+DeepSengupta-M1@users.noreply.github.com>
2022-10-21 19:43:18 +0530

    Update Assig1Q7.java

--- Assig1Q7.java
+++ Assig1Q7.java
@@ -4,7 +4,8 @@ public class Assig1Q7 {
         int n = 6;
         double sum = 0;
      
-        for(int i=1; i<=n; i++){
+        for(int i=1; i<=n; i++)
+        {
             sum = sum + 1.0/i;
         }
     System.out.println("The sum of harmonic sequence is: " + sum);
