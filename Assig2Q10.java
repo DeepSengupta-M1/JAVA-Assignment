682352603d7231b24348fbf9f0ed7178cc352623
Deep Sengupta <105095730+DeepSengupta-M1@users.noreply.github.com>
2022-11-02 22:30:13 +0530

    Add files via upload

---
+++ Assig2Q10.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Assig2Q10 {
+    public static void main(String[] args){
+        int num; 
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        num = sc.nextInt();
+
+        if(num>100 && num<50)
+        {
+            System.out.println("Number is greater than 100 but less than 50");
+        }
+        else if(num<100 && num>50)
+        {
+            System.out.println("Number is less than 100 but greater than 50");
+        }
+        else if(num<100 && num<50)
+        {
+            System.out.println("Number is less than 100 and less than 50");
+        }
+        else
+        {
+            System.out.println("Number is greater than 100 and greater than 50");
+        }
+    }
+}
---
+++ Assig2Q5.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Qty\tDescription\t\tPrice per price\tAmount");
+        System.out.println("------------------------------------------------------------");
+        System.out.println("1\tBlack Coffee\t\t\t$1.99\t$1.99");
+        System.out.println("2\tFrench Fries\t\t\t$1.65\t$1.65");
+        System.out.println("1\tCookies\t\t\t\t$1.05\t$1.05");
+        System.out.println("------------------------------------------------------------");
+        System.out.println("Total\t\t\t\t\t\t$4.69");
+    }
+}
