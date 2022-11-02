b69a2284846ae7ae556f64fdde3c109ad0d58fd5
Deep Sengupta <105095730+DeepSengupta-M1@users.noreply.github.com>
2022-11-02 23:54:02 +0530

    Add files via upload

---
+++ Assig2Q11.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Assig2Q11 {
+    public static void main(String[] args){
+        int num; 
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        num = sc.nextInt();
+
+        if (num<100)
+            System.out.println("Number is less than 100");
+        else 
+        {
+            if (num>100)
+            System.out.println("Number is greater than 100");
+        }
+    }
+}
