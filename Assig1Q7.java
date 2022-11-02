d8d7973a361da3d0eb82ea0968cae30883a57b8b
Deep Sengupta <105095730+DeepSengupta-M1@users.noreply.github.com>
2022-11-02 22:39:27 +0530

    Update Assig1Q7.java

--- Assig1Q7.java
+++ Assig1Q7.java
@@ -4,7 +4,7 @@ public class Assig1Q7 {
         int n = 6;
         double sum = 0;
      
-        for(int i=1; i<=n; i++)
+        for (int i=1; i<=n; i++)
         {
             sum = sum + 1.0/i;
         }
