bec2cf45a40661960699d5472b744a5a3e70053a
Deep Sengupta <105095730+DeepSengupta-M1@users.noreply.github.com>
2022-11-02 22:48:08 +0530

    Update Assig2Q10.java

--- Assig2Q10.java
+++ Assig2Q10.java
@@ -7,21 +7,13 @@ public class Assig2Q10 {
         System.out.print("Enter the number: ");
         num = sc.nextInt();
 
-        if(num>100 && num<50)
-        {
+        if (num>100 && num<50)
             System.out.println("Number is greater than 100 but less than 50");
-        }
-        else if(num<100 && num>50)
-        {
+        else if (num<100 && num>50)
             System.out.println("Number is less than 100 but greater than 50");
-        }
-        else if(num<100 && num<50)
-        {
+        else if (num<100 && num<50)
             System.out.println("Number is less than 100 and less than 50");
-        }
         else
-        {
             System.out.println("Number is greater than 100 and greater than 50");
-        }
     }
 }
