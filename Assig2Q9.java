46fd7c946e466630c136f40d8c16eb6f62b01ac5
Deep Sengupta <105095730+DeepSengupta-M1@users.noreply.github.com>
2022-11-02 22:05:43 +0530

    Update Assig2Q9.java

--- Assig2Q9.java
+++ Assig2Q9.java
@@ -1,13 +1,13 @@
 import java.util.*;
 
 public class Assig2Q9 {
-    public static void main(String args[]){
+    public static void main(String[] args){
         int num; 
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number: ");
         num = sc.nextInt();
 
-        if(num<100 )
+        if(num<100)
         {
             System.out.println("Number is less than 100");
         }
