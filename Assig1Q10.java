7d96b43e9c502febd8a126ff901f440e5d954b68
Deep Sengupta <105095730+DeepSengupta-M1@users.noreply.github.com>
2022-11-02 22:40:23 +0530

    Update Assig1Q10.java

--- Assig1Q10.java
+++ Assig1Q10.java
@@ -2,7 +2,7 @@ public class Assig1Q10 {
     public static void main(String[] args){
         int num1 = 0, num2 = 1, total = num1+num2;
         System.out.println("Fibonacci series: " +num1+ "," +num2+ ",");
-        for(int i=0; i<10; i++)
+        for (int i=0; i<10; i++)
         {
            total = num1 + num2;
            num1 = num2;
