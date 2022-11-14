08bc78fb6c6f88f9c859d112861ffdd4ee70af2b
Deep Sengupta <105095730+DeepSengupta-M1@users.noreply.github.com>
2022-11-14 21:04:36 +0530

    Add files via upload

---
+++ Assig3Q13.java
@@ -0,0 +1,8 @@
+public class Assig3Q13 {
+    static int x = 25;
+    static int y = 5;
+
+    public static void main(String[] args) {
+        System.out.println("The result after multiplication is: " + x * y);
+    }
+}
